280160129194529a04be3ecde5dc224dde795e61
Yulia <gallavich66@gmail.com>
2021-08-25 21:29:36 +0300

    memory

--- Task1/script.js
+++ Task1/script.js
@@ -1,41 +1,25 @@
-function range(min, max) {
-    try{
-        let sum = 0 ;
+const combinationOfLetters = (letters) =>{
+      let array = [];
+      for (let i = 0; i < letters.length; i++){
+        let string = letters.slice(0, i) + letters.slice(i + 1);
+        let fullString = combinationOfLetters(string)
 
-        min = +prompt("Enter min");
-        max = +prompt("Enter max");
-
-        for(let i = min; i <= max; i++){        
-            sum += i;
-        }
-
-        if (min > max) {
-            throw new SyntaxError(`${min} bigger then ${max}. Please, enter right order.`);
-        }
-        if (isNaN(min) || isNaN(max)) {
-            throw new SyntaxError("Please, enter the number, not string."); 
-        }
-        if (min == '' || max == '') {
-            throw new SyntaxError("The string can't be empty. Please, enter numbers."); 
-        }
-        if (min == max) {
-            throw new SyntaxError("Min can't be equal to max. Please, enter another numbers."); 
-        }
-        if (sum > Number.MAX_SAFE_INTEGER) {
-            throw new SyntaxError("It's really big sum. Please, enter another numbers."); 
-        } 
-        if(!Number.isInteger(min) || !Number.isInteger(max)){
-            throw new SyntaxError("Please, enter the integer number."); 
-        } else{
-            return alert(`Sum of numbers: ${sum}`);
-        }
-
-    } catch(err){
-        alert(err.message);
-    } finally{
-        alert("Thanks");
-    }    
+        for (let k = 0; k < fullString.length; k++){
+          array = array.concat(letters[i] + fullString[k]) 
+        }          
+      }
+      if(letters.length == 1){
+        return letters
+      }
+      if(letters.length == 0){
+        return new Error("Please, enter the string"); 
+      }else{
+        return array
+      }     
   }
 
-range();
+document.write("Result: " + combinationOfLetters("abc") )
+
+
+
 
