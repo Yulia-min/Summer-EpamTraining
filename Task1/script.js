9e59e08d69be8c40fadfc84f9618444c6e7ce359
Yulia <gallavich66@gmail.com>
2021-09-01 22:15:08 +0300

    task 7

--- Task1/script.js
+++ Task1/script.js
@@ -1,55 +1,66 @@
-/*let randn_bm = function() {  
-    let u = 0, v = 0;  
-        while (u === 0) 
-            u = Math.random();  
-        while (v === 0) 
-            v = Math.random();  
-    return Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v) * 2 | 0;
-};*/
-
-const array = [];
-const result = {};
-
-let randn_bm = function() {  
-    let u = 0, v = 0;  
-        while (u === 0) 
-            u = Math.random();  
-        while (v === 0) 
-            v = Math.random();  
-    return Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v) * 2 | 0;
-};
-
-let num = Array.from({length: 10}, randn_bm)
-
-/*let randn_bm = function(){
-    while (array.length < 10) {
-        let u = 0;
-        let v = 0;  
-            while (u === 0) {
-                u = Math.random(); 
-            }
-            while (v === 0) {
-                v = Math.random(); 
-            }
-
-    number = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v) * 2 | 0;
-
-    array.push(number);
+class Entity{
+    constructor(name){
+        this.name = name;
+    }
+    getName() {
+        return this.name
+    }
 }
-}*/
 
-let numberCount = function(){
-        for(let i = 0; i < num.length; i++){
-            let arr = num[i];
-            result[arr] = result[arr] + 1 || 1;
+class Box extends Entity {
+    staff = [];
+
+    constructor(name, staff){
+        super(name)
+        this.staff = staff;
+    }
+
+    getName(){
+        const name = super.getName()
+        if(name.length < 3){
+            return 'You need more letters '
         }
-        return result
+        return name
+    }
 }
 
-randn_bm();
-numberCount();
+class Staff extends Entity{
+    constructor(name){
+        super(name)
+    }
 
-document.write("Ten random number: " + num + "<br>");
-document.write("Result: " + JSON.stringify(result));
+    getName() {
+        return this.name
+    }
 
+    nameIncludes(str){
+        return this.getName().indexOf(str)
+    }
+}
 
+class User extends Entity{
+    box = [];
+    constructor(name, box){
+        super(name)
+        this.box = box;
+    }
+
+    getName(){
+        const name = super.getName()
+        if(name == ''){
+            return 'Say something '
+        }
+        return name
+    }
+}
+
+const boxes = new Box('A ', [20,'years']);
+console.log(boxes.getName() + boxes.staff)
+const staffs = new Staff('Hello');
+console.log(staffs.nameIncludes("Hello"))
+const users = new User('',['Yulia', 'Klepitskaya']);
+console.log(users.getName() + users.box)
+
+  
+
+ 
