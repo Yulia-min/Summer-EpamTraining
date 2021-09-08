9862d69b0b2d638db4de2dffb0119397c67b0639
Yulia <gallavich66@gmail.com>
2021-09-08 17:31:16 +0300

    Array

--- Task1/script.js
+++ Task1/script.js
@@ -1,66 +1,17 @@
-class Entity{
-    constructor(name){
-        this.name = name;
+let x =[1,2,3,4,5,6,7,8];
+
+function approx(x){
+    let y = [];
+    y.push(x[1]);
+    for(let i = 1; i < x.length-1; i++){
+        y.push(x[i - 1] + x[i + 1]);      
     }
-    getName() {
-        return this.name
-    }
+    y.push(x[x.length-2]);
+    return y
 }
 
-class Box extends Entity {
-    staff = [];
+result = approx(x);
 
-    constructor(name, staff){
-        super(name)
-        this.staff = staff;
-    }
+document.write("Array: " + x + "<br>");
+document.write("Sum of neighboring elements: " + result);
 
-    getName(){
-        const name = super.getName()
-        if(name.length < 3){
-            return 'You need more letters '
-        }
-        return name
-    }
-}
-
-class Staff extends Entity{
-    constructor(name){
-        super(name)
-    }
-
-    getName() {
-        return this.name
-    }
-
-    nameIncludes(str){
-        return this.getName().indexOf(str)
-    }
-}
-
-class User extends Entity{
-    box = [];
-    constructor(name, box){
-        super(name)
-        this.box = box;
-    }
-
-    getName(){
-        const name = super.getName()
-        if(name == ''){
-            return 'Say something '
-        }
-        return name
-    }
-}
-
-const boxes = new Box('A ', [20,'years']);
-console.log(boxes.getName() + boxes.staff)
-const staffs = new Staff('Hello');
-console.log(staffs.nameIncludes("Hello"))
-const users = new User('',['Yulia', 'Klepitskaya']);
-console.log(users.getName() + users.box)
-
-  
-
- 
