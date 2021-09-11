86f90e80c5b70fd121d2e3f19b2dc42299153edd
Yulia <gallavich66@gmail.com>
2021-09-11 14:51:28 +0300

    change task 6

--- Task1/script.js
+++ Task1/script.js
@@ -1,17 +1,25 @@
-let x =[1,2,3,4,5,6,7,8];
+const combinationOfLetters = (letters) =>{
+      let array = [];
+      for (let i = 0; i < letters.length; i++){
+        let string = letters.slice(0, i) + letters.slice(i + 1);
+        let fullString = combinationOfLetters(string)
 
-function approx(x){
-    let y = [];
-    y.push(x[1]);
-    for(let i = 1; i < x.length-1; i++){
-        y.push(x[i - 1] + x[i + 1]);      
-    }
-    y.push(x[x.length-2]);
-    return y
-}
+        for (let k = 0; k < fullString.length; k++){
+          array = array.concat(letters[i] + fullString[k]) 
+        }          
+      }
+      if(letters.length == 1){
+        return letters
+      }
+      if(letters.length == 0){
+        return new Error("Please, enter the string"); 
+      }
+      return array
+    
+  }
 
-result = approx(x);
+document.write("Result: " + combinationOfLetters("abc") )
 
-document.write("Array: " + x + "<br>");
-document.write("Sum of neighboring elements: " + result);
+
+
 
