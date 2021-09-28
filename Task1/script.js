8e99c4cec3f1c3d5b4634832a93d1f1ec1dc7159
Yulia <gallavich66@gmail.com>
2021-09-28 17:57:54 +0300

    asynch

--- Task1/script.js
+++ Task1/script.js
@@ -1,39 +1,32 @@
-const th = table.querySelectorAll('th');
-const tbody = table.querySelector('tbody');
-const row = Array.from(tbody.rows);
-const input = document.createElement('input');
-
-[].forEach.call(th, function (th, elem) {
-  th.addEventListener('click', function () {
-    sort(elem);
-  });
-});
-
-tbody.addEventListener('click', cellClick)
-
-function sort(elem) {
-
-  let compare = function (rowA, rowB) {
-    return rowA.cells[elem].innerHTML > rowB.cells[elem].innerHTML ? 1 :
-      rowA.cells[elem].innerHTML < rowB.cells[elem].innerHTML ? -1 : 0;
-  };
-
-  row.sort(compare);
-  tbody.append(...row);
+function add(){
+  const firstNumber = parseInt(document.getElementById('firstNumber').value);
+  const secondNumber = parseInt(document.getElementById('secondNumber').value);
+  setTimeout(function(){
+    document.getElementById('result').innerHTML = (firstNumber + secondNumber);
+  }, 2000);
 }
 
-function cellClick(event) { 
-  if (event.target.tagName === 'TD') {
-    input.type = 'text';
-    input.value = event.target.innerHTML;
-    input.addEventListener('blur', function(){
-      this.parentElement.textContent = this.value;
-      this.removeEventListener('blur', inputEvent);
-    });
-
-    event.target.textContent = '';
-    event.target.appendChild(input);
-
-    input.focus();
-  }
+function subtract(){
+  const firstNumber = parseInt(document.getElementById('firstNumber').value);
+  const secondNumber = parseInt(document.getElementById('secondNumber').value);
+  setTimeout(function(){
+    document.getElementById('result').innerHTML = (firstNumber - secondNumber);
+  }, 1000);
 }
+
+function multiply(){
+  const firstNumber = parseInt(document.getElementById('firstNumber').value);
+  const secondNumber = parseInt(document.getElementById('secondNumber').value);
+  setTimeout(function(){
+    document.getElementById('result').innerHTML = (firstNumber * secondNumber);
+  }, 4000);
+}
+
+function divide(){
+  const firstNumber = parseInt(document.getElementById('firstNumber').value);
+  const secondNumber = parseInt(document.getElementById('secondNumber').value);
+  setTimeout(function(){
+    document.getElementById('result').innerHTML = (firstNumber / secondNumber);
+  }, 3000);
+}
+
