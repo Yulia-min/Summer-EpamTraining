00982b613d7b3f71af606c1bcc66c149ededfe72
Yulia <gallavich66@gmail.com>
2021-09-29 23:32:28 +0300

    async

--- Task1/script.js
+++ Task1/script.js
@@ -1,32 +1,51 @@
-function add(){
-  const firstNumber = parseInt(document.getElementById('firstNumber').value);
-  const secondNumber = parseInt(document.getElementById('secondNumber').value);
-  setTimeout(function(){
-    document.getElementById('result').innerHTML = (firstNumber + secondNumber);
-  }, 2000);
+const input = document.getElementById('rpn');
+const output = document.getElementById('result');
+
+async function add(firstNumber, secondNumber){
+  await new Promise(r => setTimeout(r, 2000));
+  return firstNumber + secondNumber;
 }
 
-function subtract(){
-  const firstNumber = parseInt(document.getElementById('firstNumber').value);
-  const secondNumber = parseInt(document.getElementById('secondNumber').value);
-  setTimeout(function(){
-    document.getElementById('result').innerHTML = (firstNumber - secondNumber);
-  }, 1000);
+async function subtract(firstNumber, secondNumber){
+  await new Promise(r => setTimeout(r, 1000));
+  return firstNumber - secondNumber;
 }
 
-function multiply(){
-  const firstNumber = parseInt(document.getElementById('firstNumber').value);
-  const secondNumber = parseInt(document.getElementById('secondNumber').value);
-  setTimeout(function(){
-    document.getElementById('result').innerHTML = (firstNumber * secondNumber);
-  }, 4000);
+async function multiply(firstNumber, secondNumber){
+  await new Promise(r => setTimeout(r, 3000));
+  return firstNumber * secondNumber;
 }
 
-function divide(){
-  const firstNumber = parseInt(document.getElementById('firstNumber').value);
-  const secondNumber = parseInt(document.getElementById('secondNumber').value);
-  setTimeout(function(){
-    document.getElementById('result').innerHTML = (firstNumber / secondNumber);
-  }, 3000);
+async function divide(firstNumber, secondNumber){
+  await new Promise(r => setTimeout(r, 4000));
+  return firstNumber / secondNumber;
+}
+
+const operations = {
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide,
+};
+
+async function rpnSolver(rpnString) {
+  let stack = [];
+  
+  const tokens = rpnString.split(' ');
+  for(const token of tokens) {
+      if (token in operations) {
+          let [y, x] = [stack.pop(), stack.pop()];
+          const result = await operations[token](x, y);
+          stack.push(result);
+      } else {
+          stack.push(parseFloat(token));
+      }
+  };
+
+  return stack.pop();
+};
+
+async function rpn() {
+  output.value = await rpnSolver(input.value);
 }
 
